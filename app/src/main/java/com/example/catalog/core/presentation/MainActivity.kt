ab267b7104fca6c4d37147da2eff57961f81bd15
Rodrigo Sousa <rod.sousa899@gmail.com>
2024-05-11 19:16:07 -0300

    implemented libs and new xml to activity core

---
+++ app/src/main/java/com/example/catalog/core/presentation/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.catalog.core.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.navigation.fragment.NavHostFragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.catalog.R
+import com.example.catalog.databinding.ActivityCoreBinding
+import com.example.catalog.ui.theme.CatalogTheme
+
+class MainActivity : ComponentActivity() {
+    private val binding by viewBinding(ActivityCoreBinding::bind)
+    private lateinit var navHost: NavHostFragment
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_core)
+        navHost = binding.coreNavFragment.getFragment()
+
+        binding.bottomNav.setContent {
+            CatalogTheme {
+                TODO("bottomNAV")
+            }
+        }
+    }
+}
